175ffd17c0cdedd7efc1a4a9c235457ba9441fc6
Charles Ebong <charlesebong86@gmail.com>
2022-09-08 16:30:29 -0700

    Task 6 fixed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,22 @@
 #include<stdio.h>
+
 /**
- * main-entry point to every c program
+ * main - entry point to every c program
  *
  * Return: 0 as always for a successful program
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
 
-	printf("Size of char:%d byte(s)\n", sizeof(a));
-	printf("Size of int:%d byte(s)\n", sizeof(b));
-	printf("Size of long int:%d byte(s)\n", sizeof(c));
-	printf("Size of long long int:%d byte(s)\n", sizeof(d));
-	printf("Size of float:%d byte(s)\n", sizeof(e));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
