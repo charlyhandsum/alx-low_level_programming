d91ba173f0649fb104eb7cb9b6fb3fa19ae6df93
Charles Ebong <charlesebong86@gmail.com>
2022-09-19 16:11:26 -0700

    All Task

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* rev_string- reversing a string
+* @s: as a parameter
+* Return: always 0 for success
+*/
+
+void rev_string(char *s)
+{
+	char tmp = s[0];
+	int i, length = 0;
+
+	while (s[length] != '\0')
+		length++;
+	for (i = 0; i < length; i++)
+	{
+		length--;
+		tmp = s[i];
+		s[i] = s[length];
+		s[length] = tmp;
+	}
+}
